982ea9bdbd498785e5505b75fe8d02905c443c6f
SecureCheck Test <test@securecheck.dev>
2026-08-09 23:16:48 +0000

    test: trigger secured webhook scan

---
+++ security-test.js
@@ -0,0 +1,17 @@
+const apiKey = "sk-test-1234567890abcdef";
+const dbConn = "postgresql://admin:secretpass@db.host.com:5432/prod";
+const jwtSecret = "my-super-secret-jwt-key-2024";
+
+function searchUsers(query) {
+  return db.query(`SELECT * FROM users WHERE name = ${query}`);
+}
+
+function renderPage(userInput) {
+  document.body.innerHTML = userInput;
+}
+
+app.use(cors({ origin: '*' }));
+
+function runCode(code) {
+  return eval(code);
+}
